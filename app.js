edf7249e4f342c214024a4b898973ac4089c0df8
Leo <leodocimo@gmail.com>
2025-01-25 19:34:32 -0300

    agrega funcionalidad a la pagina

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+
+function limpiarCampos() {
+    let campo = document.getElementById("amigo").value = "";
+    document.getElementById("resultado").innerHTML = "";
+
+}
+/**
+ * post: agrega un amigo a la lista de amigos 
+ */
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (nombre !== "") {
+        listaAmigos.push(nombre);
+        mostrarAmigos();
+        limpiarCampos();
+    } else {
+        alert("Por favor, inserte un nombre");
+
+    }
+
+}
+
+/**
+ * post: muestra la lista de amigos.
+ */
+function mostrarAmigos() {
+    let elementoDeLista = document.getElementById("listaAmigos");
+    elementoDeLista.innerHTML = "";
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaAmigos[i];
+        elementoDeLista.appendChild(li);
+    }
+}
+
+/**
+ * post: muestra al amigo sorteado.
+ */
+function sortearAmigo() {
+    if (listaAmigos.length !== 0) {
+        indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        let amigoSorteado = listaAmigos[indiceAleatorio];
+        document.getElementById("listaAmigos").innerHTML = "";
+        listaAmigos = [];
+        document.getElementById("resultado").textContent = `El amigo sorteado es: ${amigoSorteado}`;
+
+    }
+}
